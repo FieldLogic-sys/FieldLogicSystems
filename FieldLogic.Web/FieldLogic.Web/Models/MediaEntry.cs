ab3e31604d867d809e3e0e0c054bb329d3aca114
Tony Aldea <tonythecoder@outlook.com>
2026-04-05 17:51:25 -0400

    Cleaned up code formatting/spacing

--- FieldLogic.Web/FieldLogic.Web/Models/MediaEntry.cs
+++ FieldLogic.Web/FieldLogic.Web/Models/MediaEntry.cs
@@ -5,14 +5,13 @@ public class MediaEntry
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Type { get; set; } = "Anime";
-    
-    
+
+
     // The C# 14 'field' keyword I am learning more about
     public int Progress
     {
         get;
         set => field = value < 0 ? 0 : value;
-
     }
 
     public string Status { get; set; } = "Watching";
